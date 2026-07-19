e78260c507850e51d3788ccaa1eddd0cffd8df49
mmnjtothez-rgb <mmnjtothez@gmail.com>
2026-07-19 23:27:46 +0330

    Update MainActivity.kt

--- app/src/main/java/com/mydgnbot/MainActivity.kt
+++ app/src/main/java/com/mydgnbot/MainActivity.kt
@@ -3,7 +3,7 @@ package com.mydgnbot
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import com.mydgnbot.ui.screens.HomeScreen
+import com.mydgnbot.ui.navigation.AppNavigation
 
 class MainActivity : ComponentActivity() {
 
@@ -11,7 +11,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
 
         setContent {
-            HomeScreen()
+            AppNavigation()
         }
     }
 }
