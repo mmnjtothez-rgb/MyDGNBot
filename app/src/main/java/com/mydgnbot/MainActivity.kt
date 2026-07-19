8f07c75e39e1941f7cd0abba3896267297f860c9
mmnjtothez-rgb <mmnjtothez@gmail.com>
2026-07-19 23:16:59 +0330

    Update MainActivity.kt

--- app/src/main/java/com/mydgnbot/MainActivity.kt
+++ app/src/main/java/com/mydgnbot/MainActivity.kt
@@ -3,15 +3,7 @@ package com.mydgnbot
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
+import com.mydgnbot.ui.screens.HomeScreen
 
 class MainActivity : ComponentActivity() {
 
@@ -19,38 +11,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
 
         setContent {
-            MyDGNApp()
-        }
-    }
-}
-
-@Composable
-fun MyDGNApp() {
-
-    MaterialTheme {
-
-        Surface(
-            modifier = Modifier.fillMaxSize()
-        ) {
-
-            Column(
-                modifier = Modifier.fillMaxSize(),
-                horizontalAlignment = Alignment.CenterHorizontally,
-                verticalArrangement = Arrangement.Center
-            ) {
-
-                Text(
-                    text = "Hello MyDGN 👋"
-                )
-
-                Text(
-                    text = "Version 0.1"
-                )
-
-                Text(
-                    text = "Pipeline Working ✔"
-                )
-            }
+            HomeScreen()
         }
     }
 }
