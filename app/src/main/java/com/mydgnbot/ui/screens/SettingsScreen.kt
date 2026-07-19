6ad12b08c3b9dc2affe8b4fbccca5a8aed5c248a
mmnjtothez-rgb <mmnjtothez@gmail.com>
2026-07-19 23:29:19 +0330

    Create SettingsScreen.kt

---
+++ app/src/main/java/com/mydgnbot/ui/screens/SettingsScreen.kt
@@ -0,0 +1,14 @@
+package com.mydgnbot.ui.screens
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun SettingsScreen(
+    onBackClick: () -> Unit
+) {
+
+    Text(
+        text = "Settings Screen"
+    )
+}
