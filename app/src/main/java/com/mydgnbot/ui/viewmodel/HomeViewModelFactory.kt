b585cced2174a9afa4856c56020aa4a36138ba25
mmnjtothez-rgb <mmnjtothez@gmail.com>
2026-07-21 00:38:39 +0330

    Update HomeViewModelFactory.kt

--- app/src/main/java/com/mydgnbot/ui/viewmodel/HomeViewModelFactory.kt
+++ app/src/main/java/com/mydgnbot/ui/viewmodel/HomeViewModelFactory.kt
@@ -3,26 +3,37 @@ package com.mydgnbot.ui.viewmodel
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import com.mydgnbot.data.repository.PlayerRepository
+import com.mydgnbot.data.repository.SettingsRepository
+
 
 class HomeViewModelFactory(
-    private val repository: PlayerRepository
+    private val playerRepository: PlayerRepository,
+    private val settingsRepository: SettingsRepository
 ) : ViewModelProvider.Factory {
+
 
     override fun <T : ViewModel> create(
         modelClass: Class<T>
     ): T {
 
+
         if (modelClass.isAssignableFrom(HomeViewModel::class.java)) {
+
 
             @Suppress("UNCHECKED_CAST")
             return HomeViewModel(
-                repository
+                playerRepository,
+                settingsRepository
             ) as T
 
+
         }
+
 
         throw IllegalArgumentException(
             "Unknown ViewModel class"
         )
+
     }
+
 }
