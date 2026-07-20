fd4814d9fa89debcf38cae27c83365fdac12c3ae
mmnjtothez-rgb <mmnjtothez@gmail.com>
2026-07-20 23:40:35 +0330

    Update MyDgnApiService.kt

--- app/src/main/java/com/mydgnbot/data/api/MyDgnApiService.kt
+++ app/src/main/java/com/mydgnbot/data/api/MyDgnApiService.kt
@@ -3,21 +3,44 @@ package com.mydgnbot.data.api
 import retrofit2.http.GET
 import retrofit2.http.Query
 
+
 interface MyDgnApiService {
 
-    @GET("api/doc")
+
+    @GET("transfers")
     suspend fun getPlayers(
+
+        @Query("user")
+        user: String,
+
+
         @Query("platform")
         platform: String,
 
-        @Query("player_type")
-        playerType: Int,
 
-        @Query("min_buy_price")
-        minimumPrice: Int,
+        @Query("timestamp")
+        timestamp: Long,
 
-        @Query("max_buy_price")
-        maximumPrice: Int
-    ): List<ApiPlayer>
+
+        @Query("hash")
+        hash: String,
+
+
+        @Query("maximumBuyOutPrice")
+        maximumBuyOutPrice: Int,
+
+
+        @Query("minimumBuyOutPrice")
+        minimumBuyOutPrice: Int,
+
+
+        @Query("botapp")
+        botApp: String,
+
+
+        @Query("playerType")
+        playerType: Int
+
+    ): ApiPlayer
 
 }
