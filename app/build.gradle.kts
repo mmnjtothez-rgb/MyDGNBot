449dd4e143255dbf35bbfb83e159ec461dfbf083
mmnjtothez-rgb <mmnjtothez@gmail.com>
2026-07-20 17:19:39 +0330

    v0.8: Add Retrofit API foundation

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -74,4 +74,23 @@ dependencies {
     implementation(
         "androidx.datastore:datastore-preferences:1.1.1"
     )
+
+
+    // Retrofit API layer
+
+    implementation(
+        "com.squareup.retrofit2:retrofit:2.11.0"
+    )
+
+    implementation(
+        "com.squareup.retrofit2:converter-gson:2.11.0"
+    )
+
+
+    // HTTP logging
+
+    implementation(
+        "com.squareup.okhttp3:logging-interceptor:4.12.0"
+    )
+
 }
