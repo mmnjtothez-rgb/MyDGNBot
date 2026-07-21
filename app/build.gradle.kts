1a876dc6b59a807afbb2705da357602c6675a977
mmnjtothez-rgb <mmnjtothez@gmail.com>
2026-07-21 22:38:55 +0330

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,6 +51,9 @@ dependencies {
         "androidx.activity:activity-compose:1.9.3"
     )
 
+implementation(
+    "io.coil-kt:coil-compose:2.7.0"
+)
     implementation(
         "androidx.compose.material3:material3:1.3.1"
     )
