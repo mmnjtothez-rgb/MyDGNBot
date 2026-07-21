91b27f6485e3a16727fe5e9e1ad0513f87758234
mmnjtothez-rgb <mmnjtothez@gmail.com>
2026-07-21 11:42:23 +0330

    Update AppContainer.kt

--- app/src/main/java/com/mydgnbot/data/AppContainer.kt
+++ app/src/main/java/com/mydgnbot/data/AppContainer.kt
@@ -2,22 +2,44 @@ package com.mydgnbot.data
 
 import android.content.Context
 import com.mydgnbot.data.datastore.SettingsDataStore
+import com.mydgnbot.data.network.ConnectivityObserver
+import com.mydgnbot.data.network.NetworkConnectivityObserver
 import com.mydgnbot.data.repository.PlayerRepository
 import com.mydgnbot.data.repository.SettingsRepository
 
+
 class AppContainer(
+
     context: Context
+
 ) {
 
+
     private val settingsDataStore =
+
         SettingsDataStore(context)
 
+
+
     val settingsRepository =
+
         SettingsRepository(
             settingsDataStore
         )
 
+
+
     val playerRepository =
+
         PlayerRepository()
+
+
+
+    val connectivityObserver: ConnectivityObserver =
+
+        NetworkConnectivityObserver(
+            context
+        )
+
 
 }
