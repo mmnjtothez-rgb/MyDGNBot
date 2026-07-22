f39ef0092457e620c037a05b7758f51018c55baf
mmnjtothez-rgb <mmnjtothez@gmail.com>
2026-07-22 14:54:28 +0330

    Update FcPlayerCard.kt

--- app/src/main/java/com/mydgnbot/ui/components/fc/FcPlayerCard.kt
+++ app/src/main/java/com/mydgnbot/ui/components/fc/FcPlayerCard.kt
@@ -1,32 +1,27 @@
 package com.mydgnbot.ui.components.fc
 
-
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
-import coil.compose.AsyncImage
-import com.mydgnbot.domain.model.Player
 
 
 @Composable
 fun FcPlayerCard(
 
-    player: Player,
+    player: com.mydgnbot.domain.model.Player,
 
     modifier: Modifier = Modifier
 
 ) {
 
-
     Box(
 
-        modifier =
-            modifier
-                .fillMaxWidth()
-                .height(300.dp)
+        modifier = modifier
+            .fillMaxWidth()
+            .height(300.dp)
 
     ) {
 
@@ -35,27 +30,8 @@ fun FcPlayerCard(
 
             cardId = 161,
 
-            modifier =
-                Modifier
-                    .matchParentSize()
-
-        )
-
-
-
-        AsyncImage(
-
-            model =
-                FutAssets.face(
-                    player.assetId
-                ),
-
-            contentDescription =
-                player.playerName,
-
-            modifier =
-                Modifier
-                    .matchParentSize()
+            modifier = Modifier
+                .matchParentSize()
 
         )
 
