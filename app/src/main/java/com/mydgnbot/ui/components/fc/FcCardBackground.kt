326d2de4946626f02087cdefc9a8f02900dbfa5a
mmnjtothez-rgb <mmnjtothez@gmail.com>
2026-07-22 12:46:26 +0330

    Update FcCardBackground.kt

--- app/src/main/java/com/mydgnbot/ui/components/fc/FcCardBackground.kt
+++ app/src/main/java/com/mydgnbot/ui/components/fc/FcCardBackground.kt
@@ -1,10 +1,10 @@
 package com.mydgnbot.ui.components.fc
 
-
-import androidx.compose.foundation.Image
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import coil.compose.rememberAsyncImagePainter
+import coil.compose.AsyncImage
+import coil.request.ImageRequest
+import androidx.compose.ui.platform.LocalContext
 
 
 @Composable
@@ -16,16 +16,18 @@ fun FcCardBackground(
 
 ) {
 
+    AsyncImage(
 
-    Image(
+        model = ImageRequest.Builder(
+            LocalContext.current
+        )
+            .data(
+                "https://cdn.futwiz.com/assets/img/fc26/items/$cardId.png"
+            )
+            .crossfade(true)
+            .build(),
 
-        painter =
-            rememberAsyncImagePainter(
-                FutAssets.cardBackground(cardId)
-            ),
-
-        contentDescription =
-            "FC Card Background",
+        contentDescription = "FC Card",
 
         modifier = modifier
 
