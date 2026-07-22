8d299462c87fdc5ccbc4bc1000e0a7aa871c6658
mmnjtothez-rgb <mmnjtothez@gmail.com>
2026-07-22 19:16:05 +0330

    Create FutGgApiService.kt

---
+++ app/src/main/java/com/mydgnbot/data/api/FutGgApiService.kt
@@ -0,0 +1,16 @@
+package com.mydgnbot.data.api
+
+import retrofit2.http.GET
+import retrofit2.http.Path
+
+interface FutGgApiService {
+
+    @GET("api/fut/players/v2/all-versions/{baseId}/")
+    suspend fun getPlayerVersions(
+
+        @Path("baseId")
+        baseId: String
+
+    ): List<FutGgPlayer>
+
+}
