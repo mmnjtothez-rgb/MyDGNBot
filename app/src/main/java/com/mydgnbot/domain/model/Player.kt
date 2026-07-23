910f5c1577408010fc5c463868b8a6eb2f81088a
mmnjtothez-rgb <mmnjtothez@gmail.com>
2026-07-23 22:07:08 +0330

    Update Player.kt

--- app/src/main/java/com/mydgnbot/domain/model/Player.kt
+++ app/src/main/java/com/mydgnbot/domain/model/Player.kt
@@ -2,60 +2,60 @@ package com.mydgnbot.domain.model
 
 data class Player(
 
-    val playerName: String,
+val playerName: String,  
 
-    val rating: Int,
+val rating: Int,  
 
-    val platform: Platform,
+val platform: Platform,  
 
-    val transactionId: String,
+val transactionId: String,  
 
-    val tradeId: String,
+val tradeId: String,  
 
-    val assetId: String,
+val assetId: String,  
 
-    val resourceId: String,
+val resourceId: String,  
 
-    val baseId: String,
+val baseId: String,  
 
-    val startPrice: Int,
+val startPrice: Int,  
 
-    val buyNowPrice: Int,
+val buyNowPrice: Int,  
 
-    val cardValue: Int,
+val cardValue: Int,  
 
-    val payment: Double,
+val payment: Double,  
 
-    val chemistryStyle: String,
+val chemistryStyle: String,  
 
-    val owners: Int,
+val owners: Int,  
 
-    val marketExpiry: Long,
+val marketExpiry: Long,  
 
-    val status: String,
+val status: String,  
 
-    // ---------- FUT.GG ----------
+// ---------- FUT.GG ----------  
 
-    val overall: Int? = null,
+val overall: Int? = null,  
 
-    val rarity: String? = null,
+val rarity: String? = null,  
 
-    val imageUrl: String? = null,
+val imageUrl: String? = null,  
 
-    val compactImageUrl: String? = null,
+val compactImageUrl: String? = null,  
 
-    val nationId: Int? = null,
+val nationId: Int? = null,  
 
-    val leagueId: Int? = null,
+val leagueId: Int? = null,  
 
-    val clubId: Int? = null,
+val clubId: Int? = null,  
 
-    val skillMoves: Int? = null,
+val skillMoves: Int? = null,  
 
-    val weakFoot: Int? = null,
+val weakFoot: Int? = null,  
 
-    val preferredFoot: Int? = null,
+val preferredFoot: Int? = null,  
 
-    val position: Int? = null
+val position: Int? = null
 
 )
