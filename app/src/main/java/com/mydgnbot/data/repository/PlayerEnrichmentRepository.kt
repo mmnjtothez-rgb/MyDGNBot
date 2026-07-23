00dd8653059290d2050315a5810a29a3482ac41a
mmnjtothez-rgb <mmnjtothez@gmail.com>
2026-07-23 09:39:15 +0330

    Create PlayerEnrichmentRepository.kt

---
+++ app/src/main/java/com/mydgnbot/data/repository/PlayerEnrichmentRepository.kt
@@ -0,0 +1,78 @@
+package com.mydgnbot.data.repository
+
+import com.mydgnbot.data.api.ApiPlayer
+import com.mydgnbot.data.mapper.ApiPlayerMapper.toPlayer
+import com.mydgnbot.domain.model.Player
+
+class PlayerEnrichmentRepository(
+
+    private val futGgRepository: FutGgRepository = FutGgRepository()
+
+) {
+
+    suspend fun enrich(
+
+        apiPlayer: ApiPlayer
+
+    ): Player {
+
+        val player =
+
+            apiPlayer.toPlayer()
+
+        val futPlayer =
+
+            futGgRepository.getPlayerVersion(
+
+                baseId = player.baseId,
+
+                assetId = player.assetId
+
+            )
+
+        return if (
+
+            futPlayer != null
+
+        ) {
+
+            player.copy(
+
+                overall =
+                    futPlayer.overall,
+
+                rarity =
+                    futPlayer.rarityGroupName,
+
+                imageUrl =
+                    futPlayer.imageUrl,
+
+                compactImageUrl =
+                    futPlayer.compactImageUrl,
+
+                nationId =
+                    futPlayer.nationEaId,
+
+                leagueId =
+                    futPlayer.leagueEaId,
+
+                clubId =
+                    futPlayer.clubEaId,
+
+                skillMoves =
+                    futPlayer.skillMoves,
+
+                weakFoot =
+                    futPlayer.weakFoot
+
+            )
+
+        } else {
+
+            player
+
+        }
+
+    }
+
+}
