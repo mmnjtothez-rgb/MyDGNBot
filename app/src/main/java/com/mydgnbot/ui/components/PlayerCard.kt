1c150251a4874c287a84e37b7d3a8107b369b6b8
mmnjtothez-rgb <mmnjtothez@gmail.com>
2026-07-24 10:51:21 +0330

    Update PlayerCard.kt

--- app/src/main/java/com/mydgnbot/ui/components/PlayerCard.kt
+++ app/src/main/java/com/mydgnbot/ui/components/PlayerCard.kt
@@ -8,7 +8,6 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
-import com.mydgnbot.ui.util.ChemistryStyleIcon
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.*
