34a46422eb68e2f1063dc31cee025faf862faada
mmnjtothez-rgb <mmnjtothez@gmail.com>
2026-07-26 22:50:47 +0330

    Update MainActivity.kt

--- app/src/main/java/com/mydgnbot/MainActivity.kt
+++ app/src/main/java/com/mydgnbot/MainActivity.kt
@@ -3,17 +3,34 @@ package com.mydgnbot
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import com.mydgnbot.ui.MyDGNApp
+import androidx.activity.enableEdgeToEdge
+import androidx.core.view.WindowCompat
+import com.mydgnbot.ui.navigation.AppNavigation
+import com.mydgnbot.ui.theme.MyDGNBotTheme
 
 class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
+
+        enableEdgeToEdge()
+
+        WindowCompat.setDecorFitsSystemWindows(
+            window,
+            false
+        )
 
         setContent {
 
-            MyDGNApp()
+            MyDGNBotTheme {
+
+                AppNavigation()
+
+            }
 
         }
+
     }
+
 }
