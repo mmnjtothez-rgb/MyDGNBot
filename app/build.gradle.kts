4a2e1d69c49cf55f5a942f6589d236985c342d76
mmnjtothez-rgb <mmnjtothez@gmail.com>
2026-07-28 13:18:54 +0330

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,67 +33,37 @@ android {
     }
 
     buildTypes {
-
         release {
-
             isMinifyEnabled = false
-
         }
-
     }
 }
 
 dependencies {
-
     implementation("androidx.core:core-ktx:1.15.0")
 
-    implementation(
-        "androidx.activity:activity-compose:1.9.3"
-    )
+    implementation("androidx.activity:activity-compose:1.9.3")
 
-implementation(
-    "io.coil-kt:coil-compose:2.7.0"
-)
-    implementation(
-        "androidx.compose.material3:material3:1.3.1"
-    )
+    implementation("io.coil-kt:coil-compose:2.7.0")
 
-    implementation(
-        "androidx.compose.ui:ui:1.7.5"
-    )
+    implementation("androidx.compose.material3:material3:1.3.1")
 
-    implementation(
-        "androidx.compose.ui:ui-tooling-preview:1.7.5"
-    )
+    implementation("androidx.compose.ui:ui:1.7.5")
 
-    debugImplementation(
-        "androidx.compose.ui:ui-tooling:1.7.5"
-    )
+    implementation("androidx.compose.ui:ui-tooling-preview:1.7.5")
 
-    implementation(
-        "androidx.navigation:navigation-compose:2.8.4"
-    )
+    debugImplementation("androidx.compose.ui:ui-tooling:1.7.5")
 
-    implementation(
-        "androidx.datastore:datastore-preferences:1.1.1"
-    )
+    implementation("androidx.navigation:navigation-compose:2.8.4")
 
+    implementation("androidx.datastore:datastore-preferences:1.1.1")
+
+    implementation("com.google.android.material:material:1.12.0")
 
     // Retrofit API layer
-
-    implementation(
-        "com.squareup.retrofit2:retrofit:2.11.0"
-    )
-
-    implementation(
-        "com.squareup.retrofit2:converter-gson:2.11.0"
-    )
-
+    implementation("com.squareup.retrofit2:retrofit:2.11.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.11.0")
 
     // HTTP logging
-
-    implementation(
-        "com.squareup.okhttp3:logging-interceptor:4.12.0"
-    )
-
+    implementation("com.squareup.okhttp3:logging-interceptor:4.12.0")
 }
