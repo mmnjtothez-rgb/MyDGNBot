9cfca990608e03b852872dbdad5f708dc7d56272
mmnjtothez-rgb <mmnjtothez@gmail.com>
2026-07-29 19:46:04 +0330

    Update SettingsViewModelFactory.kt

--- app/src/main/java/com/mydgnbot/ui/viewmodel/SettingsViewModelFactory.kt
+++ app/src/main/java/com/mydgnbot/ui/viewmodel/SettingsViewModelFactory.kt
@@ -8,22 +8,11 @@ class SettingsViewModelFactory(
     private val repository: SettingsRepository
 ) : ViewModelProvider.Factory {
 
-
-    override fun <T : ViewModel> create(
-        modelClass: Class<T>
-    ): T {
-
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
         if (modelClass.isAssignableFrom(SettingsViewModel::class.java)) {
-
             @Suppress("UNCHECKED_CAST")
-            return SettingsViewModel(
-                repository
-            ) as T
-
+            return SettingsViewModel(repository) as T
         }
-
-        throw IllegalArgumentException(
-            "Unknown ViewModel class"
-        )
+        throw IllegalArgumentException("Unknown ViewModel class")
     }
 }
