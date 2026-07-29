9e1d144e8ff411a6546911dce199a906b531ed48
mmnjtothez-rgb <mmnjtothez@gmail.com>
2026-07-29 19:46:38 +0330

    Update PlayerMethod.kt

--- app/src/main/java/com/mydgnbot/domain/model/PlayerMethod.kt
+++ app/src/main/java/com/mydgnbot/domain/model/PlayerMethod.kt
@@ -4,31 +4,18 @@ enum class PlayerMethod(
     val apiValue: Int,
     val displayName: String
 ) {
-
     SAFE(
         apiValue = 1,
         displayName = "Safe Method"
     ),
-
     QUICKSELL(
         apiValue = 2,
         displayName = "Quicksell Method"
     );
 
     companion object {
-
-        fun fromValue(
-            value: Int
-        ): PlayerMethod {
-
-            return entries.firstOrNull {
-
-                it.apiValue == value
-
-            } ?: QUICKSELL
-
+        fun fromValue(value: Int): PlayerMethod {
+            return entries.firstOrNull { it.apiValue == value } ?: QUICKSELL
         }
-
     }
-
 }
