4f9b62c171ab3b20e7e57fe8e77853c993e56713
mmnjtothez-rgb <mmnjtothez@gmail.com>
2026-07-30 21:30:24 +0330

    Update BotStatusCard.kt

--- app/src/main/java/com/mydgnbot/ui/components/BotStatusCard.kt
+++ app/src/main/java/com/mydgnbot/ui/components/BotStatusCard.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
+import com.mydgnbot.ui.components.BotStatus
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
