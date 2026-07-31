00c889bc76c7e4bc8be2229584b3b0fafbdc76cf
mmnjtothez-rgb <mmnjtothez@gmail.com>
2026-07-31 19:09:27 +0330

    Update Color.kt

--- app/src/main/java/com/mydgnbot/ui/theme/Color.kt
+++ app/src/main/java/com/mydgnbot/ui/theme/Color.kt
@@ -2,24 +2,17 @@ package com.mydgnbot.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Black0 = Color(0xFF000000)
-val Black1 = Color(0xFF030605)
-val Black2 = Color(0xFF07110F)
+val Black0 = Color(0xFF020503)
+val Black1 = Color(0xFF090F0C)
+val TextPrimary = Color(0xFFF3F4F6)
+val TextSecondary = Color(0xFF9CA3AF)
+val TextMuted = Color(0xFF9CA3AF)
 
-val MidnightGreen1 = Color(0xFF003D32)
-val MidnightGreen2 = Color(0xFF001B16)
-val MidnightGreen3 = Color(0xFF00110E)
-
-val Emerald = Color(0xFF18E6BE)
-val EmeraldSoft = Color(0xFF2CFFB9)
-val EmeraldMuted = Color(0xFF0F5E4E)
-
-val Titanium = Color(0xFF0E1415)
-val Titanium2 = Color(0xFF101819)
-val Line = Color(0xFF28D7AF)
-
-val TextPrimary = Color(0xFFF2F6F5)
-val TextSecondary = Color(0xFF9FB3AE)
-val TextMuted = Color(0xFF6F817C)
-
-val Gold = Color(0xFFF2C66D)
+val Emerald = Color(0xFF42E8B4)
+val EmeraldDark = Color(0xFF28C795)
+val EmeraldGlow = Color(0xFF0A2417)
+val DarkBg = Color(0xFF020503)
+val ContainerBgTop = Color(0xFF0B1711)
+val ContainerBgBottom = Color(0xFF040A07)
+val BorderTop = Color(0xFF1E4230)
+val BorderBottom = Color(0xFF0A160F)
